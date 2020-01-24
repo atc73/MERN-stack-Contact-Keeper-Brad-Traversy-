e6b6a18d9f6804c9574c3ad4e087a22e062cd7cc
atc73 <joris.sellam@gmail.com>
2020-01-24 15:18:27 +0100

    prepare for deploy

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+// Connect Database
+connectDB();
 
+// Init Middleware
+app.use(express.json({ extented: false }));
 
-app.get('/', (req, res) => res.json({ "msg": "Welcome to Paradise" }));
 
 
 // Define Routes
@@ -13,12 +18,13 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(epress.static('client/build'));
 
-
-
-
-
-
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
